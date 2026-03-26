a6e2afa3d7f3a38f17bd18102ef4cd8cc71e9889
Aziz Rmadi <azizrmadi@gmail.com>
2026-03-26 17:54:58 +0100

    finished chapter 2

--- caesar/src/Lib.hs
+++ caesar/src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
   ( lowerAlphabet,
     upperAlphabet,
     digits,
+    rot135,
   )
 where
 
@@ -15,3 +16,39 @@ upperAlphabet = ['A' .. 'Z']
 
 digits :: Alphabet
 digits = ['0' .. '9']
+
+indexOf :: Char -> Alphabet -> Int
+indexOf _ [] = -1
+indexOf c (x : xs) = if x == c then 0 else 1 + indexOf c xs
+
+isLower :: Char -> Bool
+isLower c = c `elem` lowerAlphabet
+
+isUpper :: Char -> Bool
+isUpper c = c `elem` upperAlphabet
+
+isDigit :: Char -> Bool
+isDigit c = c `elem` digits
+
+alphabetRot :: Alphabet -> Int -> Char -> Char
+alphabetRot alphabet n ch =
+  alphabet !! ((indexOf ch alphabet + n) `mod` length alphabet)
+
+upperRot :: Int -> Char -> Char
+upperRot n ch = alphabetRot upperAlphabet n ch
+
+lowerRot :: Int -> Char -> Char
+lowerRot n ch = alphabetRot lowerAlphabet n ch
+
+digitRot :: Int -> Char -> Char
+digitRot n ch = alphabetRot digits n ch
+
+rot135Char :: Char -> Char
+rot135Char ch
+  | isUpper ch = upperRot 13 ch
+  | isLower ch = lowerRot 13 ch
+  | isDigit ch = digitRot 5 ch
+  | otherwise = ch
+
+rot135 :: String -> String
+rot135 message = map (rot135Char) message
