a151335d00a29aa358a617c35814f7bc146da3e1
Aziz Rmadi <azizrmadi@gmail.com>
2026-04-03 16:15:06 +0200

    CLI arguments parsing done

--- lnums/app/Main.hs
+++ lnums/app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
-import Lib
+import System.Environment (getArgs, getProgName)
+
+printHelpText :: String -> IO ()
+printHelpText msg = do
+  putStrLn (msg ++ "\n")
+  progName <- getProgName
+  putStrLn ("Usage: " ++ progName ++ " <filename>")
+
+parseArguments :: [String] -> Maybe FilePath
+parseArguments [filePath] = Just filePath
+parseArguments _ = Nothing
 
 main :: IO ()
-main = someFunc
+main = do
+  cliArgs <- getArgs
+  let mFilePath = parseArguments cliArgs
+  maybe (printHelpText "Missing filename") (\filePath -> putStrLn filePath) mFilePath
